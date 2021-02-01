e48c30e6a5c53877b9cd640e4f1c1fbf625b3960
kemoelamorim <kemoel.amorim@gmail.com>
2021-02-01 08:52:41 -0300

    criando app

--- testes/teste_gerenciador.py
+++ testes/teste_gerenciador.py
@@ -1,7 +1,9 @@
 from starlette.testclient import TestClient
-from starlette.status import HTTP_200_OK
+from starlette.status_code import HTTP_200_OK
+from gerenciador_tarefas.gerenciador import app
+
 
 def test_quando_listar_tarefas_devo_retornar_codigo_de_status_ok():
     cliente = TestClient(app)
     resposta = cliente.get("/tarefas")
-    assert resposta.status == HTTP_200_OK
+    assert resposta.status_code == HTTP_200_OK
